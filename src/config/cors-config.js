87668c5d9e76da46324b38b4f6526cc6b236c35a
zakia <zakiakhanum105@gmail.com>
2025-06-27 22:43:42 +0500

    origin whitelisted

--- src/config/cors-config.js
+++ src/config/cors-config.js
@@ -2,7 +2,7 @@ const cors = require('cors');
 
 // Configure CORS options for public access
 const corsOptions = {
-  origin: true, // Allow all origins
+  origin: ['https://www.leasy.com.pl'], // Allow all origins
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
